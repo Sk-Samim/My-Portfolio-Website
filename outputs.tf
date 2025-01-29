d18c6f420601aeeefcd1627b6984be3c256d9f5e
Sk Samim <sekhsamim187@gmail.com>
2025-01-30 01:09:26 +0530

    Updated output values

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "websiteEndPoint" {
-  value = "http://${aws_s3_bucket_website_configuration.website.website_endpoint}"
+# output "websiteEndPoint" {
+#   value = "http://${aws_s3_bucket_website_configuration.website.website_endpoint}"
+# }
+
+output "cloudfront_endpoint" {
+  value = "https://${aws_cloudfront_distribution.s3_distribution.domain_name}"
 }
