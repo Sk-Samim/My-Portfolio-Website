52b18f6f2d104832657f673e189465222e906e92
Sk Samim <sekhsamim187@gmail.com>
2025-01-29 23:52:46 +0530

    Integrated CloudFront to redirect s3 static website endpoint http to https

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ resource "aws_s3_bucket_public_access_block" "PublicAccess" {
   depends_on = [aws_s3_bucket.mybucket]
   bucket     = aws_s3_bucket.mybucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 #Configuring ownership control for the bucket
@@ -31,28 +31,40 @@ resource "aws_s3_bucket_ownership_controls" "OwnershipControl" {
 #Configuring access control for the bucket
 resource "aws_s3_bucket_acl" "Access-Control" {
   bucket = aws_s3_bucket.mybucket.id
-  acl    = "public-read"
+  acl    = "private"
 
   depends_on = [aws_s3_bucket_ownership_controls.OwnershipControl]
 }
 
-#Configuring bucket policy
-resource "aws_s3_bucket_policy" "bucketPolicy" {
+# Calling the data source for fetching the current aws account id
+data "aws_caller_identity" "current" {}
+
+#Configuring bucket policy to allow CloudFront access via OAC
+resource "aws_s3_bucket_policy" "cloudfront_bucket_policy" {
   bucket = aws_s3_bucket.mybucket.id
 
   policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
       {
+        "Sid" : "AllowCloudFrontServicePrincipalReadOnly",
         "Effect" : "Allow",
-        "Principal" : "*",
+        "Principal" : {
+          "Service" : "cloudfront.amazonaws.com"
+        },
         "Action" : "s3:GetObject",
-        "Resource" : "arn:aws:s3:::${local.bucket_name}/*"
+        "Resource" : "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket}/*",
+        "Condition" : {
+          "StringEquals" : {
+            "AWS:SourceArn" : "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
+          }
+        }
       }
     ]
   })
 }
 
+# Calling this below module to upload multiple files 
 module "template_files" {
   source   = "hashicorp/dir/template"
   version  = "1.0.2"
@@ -81,7 +93,60 @@ resource "aws_s3_object" "hosting_bucket_files" {
   content_type = each.value.content_type
   source       = each.value.source_path
   content      = each.value.content
-  acl          = "public-read"
+  acl          = "private"
   depends_on   = [aws_s3_bucket_acl.Access-Control]
-  etag         = filemd5(each.value.source_path) # Adding this line to evaluate change to the objects by terraform
+  etag         = filemd5(each.value.source_path) # Re-upload files if content changes
+}
+
+# Creating Origin Access Control (OAC) for CloudFront to securely access the S3 bucket
+resource "aws_cloudfront_origin_access_control" "OAC" {
+  name                              = "my-oac"
+  description                       = "OAC for accessing the S3 bucket via CloudFront"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# CloudFront Distribution for HTTPS redirection with OAC
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  enabled = true
+
+  origin {
+    domain_name              = aws_s3_bucket.mybucket.bucket_regional_domain_name
+    origin_id                = "S3-${aws_s3_bucket.mybucket.id}"
+    origin_access_control_id = aws_cloudfront_origin_access_control.OAC.id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.mybucket.id}"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true # Use CloudFront's default SSL certificate
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  default_root_object = "index.html"
+
+  tags = {
+    Name = "CloudFront HTTPS for S3"
+  }
+
+  depends_on = [aws_s3_bucket_website_configuration.website]
 }
