f532e1fd99d5d5e6186d664252bf244689e0714e
Sk Samim <sekhsamim187@gmail.com>
2025-02-12 04:41:13 +0530

    Modified the certification section also added a contact form

--- Website/script.js
+++ Website/script.js
@@ -1,6 +1,20 @@
+// This section is for mobile menu toggle
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// This section is for sending email
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  emailjs.sendForm('service_o231sgh', 'template_1ibfnam', this)
+    .then(function (response) {
+      alert('Email sent successfully');
+      document.getElementById('contact-form').reset();
+    }, function (error) {
+      alert('Email sending failed');
+    });
+});
